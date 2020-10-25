6f6a24bd16276861264bce1efc83a97a3aad45fe
scratchyone <scratchywon@gmail.com>
2020-10-24 23:43:47 -0400

    Finish connecting to API

--- components/main.ts
+++ components/main.ts
@@ -1,13 +1,11 @@
-export const API_URL = 'http://localhost:80';
+export const API_URL = process.env.NEXT_PUBLIC_API_URL;
 export interface Capability {
   expire: number;
   type: 'reminders';
   user: string;
 }
-export async function getCapabilityInfo(
-  token: string
-): Promise<null | Capability> {
+export async function getCapabilityInfo(token: string): Promise<Capability> {
   const res = await fetch(API_URL + '/capabilities/' + token);
-  if (res.status === 404) return null;
+  if (res.status === 404) throw new Error((await res.json()).error);
   return await res.json();
 }
--- pages/reminders/[id].tsx
+++ pages/reminders/[id].tsx
@@ -3,14 +3,46 @@ import React, { useState, useEffect } from 'react';
 import { FaTimes } from 'react-icons/fa';
 import classnames from 'classnames';
 import { useRouter } from 'next/router';
-import { Capability, getCapabilityInfo } from '../../components/main';
+import { API_URL, getCapabilityInfo } from '../../components/main';
 import { formatDistanceToNow, formatDistanceToNowStrict } from 'date-fns';
+import useSWR, { mutate } from 'swr';
+interface Reminder {
+  text: string;
+  time: number;
+  id: string;
+}
 function useForceUpdate() {
   console.log('updated');
   const [value, setValue] = useState(0); // integer state
   return () => setValue((value) => ++value); // update the state to force render
 }
-function Reminder(props): React.ReactElement {
+async function getReminders(
+  token: string,
+  user: string
+): Promise<Array<Reminder>> {
+  const res = await fetch(API_URL + '/' + user + '/reminders/', {
+    headers: { authorization: `Bearer ${token}` },
+  });
+  if (res.status !== 200) throw new Error((await res.json()).error);
+  return await res.json();
+}
+async function deleteReminder(
+  token: string,
+  user: string,
+  id: string
+): Promise<Array<Reminder>> {
+  const res = await fetch(API_URL + '/' + user + '/reminders/' + id, {
+    headers: { authorization: `Bearer ${token}` },
+    method: 'DELETE',
+  });
+  if (res.status !== 200) throw new Error((await res.json()).error);
+  return await res.json();
+}
+function Reminder(props: {
+  reminder: Reminder;
+  id: string;
+  user: string;
+}): React.ReactElement {
   const forceUpdate = useForceUpdate();
   let formattedTime = formatDistanceToNowStrict(new Date(props.reminder.time), {
     addSuffix: true,
@@ -30,45 +62,52 @@ function Reminder(props): React.ReactElement {
         );
         forceUpdate();
       }
-    }, 1000);
+    }, 500);
   }, []);
-  return (
+  return props.reminder.time > Date.now() ? (
     <div className={styles.reminder}>
-      <FaTimes className={styles.x} />
+      <FaTimes
+        className={styles.x}
+        onClick={async () => {
+          mutate(
+            `reminders/${props.id}`,
+            async (reminders: Reminder[]) =>
+              reminders.filter((n) => n.id !== props.reminder.id),
+            false
+          );
+          await deleteReminder(props.id, props.user, props.reminder.id);
+          mutate(`reminders/${props.id}`);
+        }}
+      />
       <span>
         <span className={styles.reminderText}>{props.reminder.text}</span>
         <span className={styles.reminderTime}>{formattedTime}</span>
       </span>
     </div>
+  ) : (
+    <div></div>
   );
 }
 export default function Reminders(): React.ReactElement {
   const router = useRouter();
-  const [capInfo, setCapInfo] = useState<Capability | undefined | null>(
-    undefined
+  const id = router.query['id'];
+  const { data: capInfo, error: capError } = useSWR(
+    `capabilities/${id}`,
+    async () => await getCapabilityInfo(id as string)
   );
-  const { id } = router.query;
-  console.log(id);
-  useEffect(
-    () =>
-      (async () => {
-        const res = await getCapabilityInfo(id as string);
-        setCapInfo(res);
-      })() && null,
-    [id]
+  const { data: reminders, error: rmError } = useSWR(
+    capInfo ? `reminders/${id}` : null,
+    async () => await getReminders(id as string, capInfo?.user || ''),
+    { refreshInterval: 2000 }
   );
-  const reminders: { text: string; time: number; id: string }[] = [
-    { text: 'Clean room', time: 1603577209438, id: 'dsd' },
-    { text: 'Deal with the dog', time: 1603576391000, id: '333' },
-  ];
-  const expired = capInfo === null;
+  const expired = !!capError;
   return expired ? (
     <div className={styles.background_expired}>
       <div className={styles.expired_card}>
         <div className={styles.expired_text}>This link has expired</div>
       </div>
     </div>
-  ) : capInfo !== undefined ? (
+  ) : capInfo ? (
     <div className={styles.background}>
       <div className={styles.wrapper}>
         <div className={styles.header}>Reminders</div>
@@ -81,9 +120,22 @@ export default function Reminders(): React.ReactElement {
               }).replace('about ', '')}
             </span>
           </div>
-          {reminders.map((reminder) => (
-            <Reminder reminder={reminder} key={reminder.id} />
-          ))}
+          {rmError && (
+            <div className={styles.reminder}>
+              <span className={classnames(styles.reminderText, styles.expiry)}>
+                Failed to fetch reminders, something is wrong
+              </span>
+            </div>
+          )}
+          {reminders &&
+            reminders.map((reminder) => (
+              <Reminder
+                reminder={reminder}
+                key={reminder.id}
+                id={id as string}
+                user={capInfo.user}
+              />
+            ))}
         </div>
       </div>
     </div>
