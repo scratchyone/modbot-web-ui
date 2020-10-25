d338bdab18924fae8de8fa16439dcb232e06f998
scratchyone <scratchywon@gmail.com>
2020-10-25 19:10:37 -0400

    Add favicon

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,11 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link
+            rel="icon"
+            type="image/png"
+            href="https://cdn.discordapp.com/avatars/738517864016773241/45ceee959e8bc1a79584aaf73d01ff68.png"
+          />
+          <link
             href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap"
             rel="stylesheet"
           />
