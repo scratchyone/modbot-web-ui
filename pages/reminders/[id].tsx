fc180d8b2e9f300da700a56a7584ebee5eae156f
scratchyone <scratchywon@gmail.com>
2020-10-25 18:56:39 -0400

    Remove log statement

--- pages/reminders/[id].tsx
+++ pages/reminders/[id].tsx
@@ -19,7 +19,6 @@ interface Reminder {
   id: string;
 }
 function useForceUpdate() {
-  console.log('updated');
   const [value, setValue] = useState(0); // integer state
   return () => setValue((value) => ++value); // update the state to force render
 }
